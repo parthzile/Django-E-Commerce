701020ff9d65bcd1848fc567ee1b3effe30a3341
parthzile <parthzile01@gmail.com>
2021-05-17 17:38:07 +0530

    Update

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -12,7 +12,7 @@ class Customer(models.Model):
     
 class Product(models.Model):
     name = models.CharField(max_length=200)
-    price = models.FloatField()
+    price = models.DecimalField(max_digits=7,decimal_places=2)
     digital = models.BooleanField(default=False,null=True, blank=True)  #To check of a product is a virtual  product or physical product.
     image = models.ImageField(null=True, blank=True)
     def __str__(self):
