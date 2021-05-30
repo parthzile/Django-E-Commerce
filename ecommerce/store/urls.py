5fa748a98a159262f9cb64aad7963a6a4c0e50ab
parthzile <parthzile01@gmail.com>
2021-05-30 09:23:00 +0530

    Removed Login Func

--- ecommerce/store/urls.py
+++ ecommerce/store/urls.py
@@ -4,8 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.store, name="store"),
     path("localstore/", views.localstore, name="store"),
-    path("login/", views.login, name="login"),
-    path("register/", views.register, name="register"),
     path("cart/", views.cart, name="cart"),
     path("checkout", views.checkout, name="checkout"),
     path('update_item/', views.updateItem, name="update_item"),
